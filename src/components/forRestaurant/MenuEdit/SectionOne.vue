66f0757ba12c6681da53c6701ca91532d2a787f9
panda1 <blueblue0011@gmail.com>
2022-09-12 15:15:03 -0700

    making add menu button

---
+++ src/components/forRestaurant/MenuEdit/SectionOne.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <input v-model="this.section_one_name" placeholder="section name">
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                section_one:[{
+                    img_url: "",
+                    name: "",
+                    the_disc: "",
+                    price: "",
+                }],
+                section_one_name: "",
+
+            }
+        },
+        name: 'section-one'
+    }
+</script>
+
+<style scoped>
+
+</style>
